649b7a62fb501348c60f4ec06535f6ddec24532d
Teknaw <145044725+Teknaw@users.noreply.github.com>
2025-11-29 02:22:28 -0800

    bago ulit hehe

---
+++ Sumulong Enterprise/Forms/Reports/InventoryOverviewForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Sumulong_Enterprise.Forms.Reports
+{
+    partial class InventoryOverviewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(202, 220);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(391, 150);
+            dataGridView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(50, 39);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 100);
+            panel1.TabIndex = 1;
+            panel1.Paint += this.panel1_Paint;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(536, 39);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(200, 100);
+            panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(293, 39);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(200, 100);
+            panel3.TabIndex = 3;
+            // 
+            // InventoryOverviewForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(dataGridView1);
+            Name = "InventoryOverviewForm";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+    }
+}
---
+++ Sumulong Enterprise/Forms/Reports/LowStockForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sumulong_Enterprise.Forms.Reports
+{
+    public partial class StockPerLocationForm : Form
+    {
+        public StockPerLocationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            ((ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(79, 27);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(529, 293);
+            dataGridView1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(275, 358);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 58);
+            button1.TabIndex = 4;
+            button1.Text = "Restart";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // StockPerLocationForm
+            // 
+            ClientSize = new Size(694, 494);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "StockPerLocationForm";
+            ((ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+
+        }
+        private DataGridView dataGridView1;
+        private Button button1;
+    }
+}
